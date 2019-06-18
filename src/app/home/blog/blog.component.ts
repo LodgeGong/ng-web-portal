ae4c4f3419448c91c067e27e77bed0a7d7356fdc
gongyh <gongyh@tingyun.com>
2019-06-18 08:52:19 +0800

    日志模块架构

--- src/app/home/blog/blog.component.ts
+++ src/app/home/blog/blog.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
+  titles:string[]=['吴军谷歌方法论感想','吴军硅谷来信随笔','格斗技能复课'];
+
   constructor() { }
 
   ngOnInit() {
--- src/app/home/home-center/home-center.component.ts
+++ src/app/home/home-center/home-center.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-home-center',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeCenterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
   }
 
+  toChildModule(path:string){
+    this.router.navigate(['/home/'+path]);
+  }
 }
